f4ce888c4d27fd04e7c1c979bfdf1e564332263a
pd212 <74095344+pd212@users.noreply.github.com>
2023-07-05 18:39:37 +0530

    Add files via upload

---
+++ src/AnalogClock.js
@@ -0,0 +1,52 @@
+import React, {useState, useEffect} from 'react'
+import "./AnalogClock.css";
+
+
+const AnalogClock = () => {
+    const [time , setTime] = useState(new Date());
+
+    useEffect( () => {
+        const intervalId = setInterval( () =>{
+            setTime(new Date());
+        }, 1000);
+
+
+        return () =>{
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    const hours = time.getHours()%12;
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+
+    const minuteStyle = {
+        transform : `rotate(${(minutes / 60 ) * 360}deg)`,
+    };
+    const secondStyle = {
+        transform: `rotate(${(seconds / 60) * 360}deg)`,
+    };
+    const hourStyle = {
+        transform : `rotate(${(hours / 12 ) * 360}deg)`,
+    };
+   
+    
+
+
+  return (
+    <div>
+        <div className='clock'>
+            <div className='hand hour-hand' style={hourStyle}></div>
+            <div className='hand minute-hand' style={minuteStyle}></div>
+            <div className='hand second-hand' style={secondStyle}></div>
+        </div>
+        <div className='digital-clock'>
+            <span className='digital-hour'>{hours +":"}</span>
+            <span className='digital-minutes'>{minutes+":"}</span>
+            <span className='digital-seconds'>{seconds}</span>
+        </div>
+    </div>
+  )
+}
+
+export default AnalogClock;
